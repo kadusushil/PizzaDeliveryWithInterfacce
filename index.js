c44663fe1c2f2367675a26d17b4fd1651e058950
sushilkadu <sushil@appalladium.com>
2018-09-24 13:50:47 +0530

    Adding code to access publis assets

--- index.js
+++ index.js
@@ -13,28 +13,6 @@ const server = require('./lib/server');
 var app = {};
 
 app.init = () => {
-  // init data module
-  // _data.init();
-  // TODO only for testing and should be removed.
-  // _data.createFile('users', 'abcdef', {'abc':123}, (error)=>{
-  //   console.log('Received file status: ', error);
-  //   if (!error) {
-  //     _data.listFiles('users', false, (error, fileNames)=> {
-  //       if (!error) {
-  //         fileNames.forEach((fileName)=> {
-  //           console.log('FILE: ', fileName);
-  //         });
-  //       }
-  //     });
-  //   }
-  // });
-  // _data.updateFile('users', 'testing', {'def':456}, (error)=> {
-  //   console.log('The file is updated: ', error);
-  // });
-  //
-  // _data.readFile('users', 'testing', (error, data)=> {
-  //   console.log('File content: ', data);
-  // });
   server.init();
 }
 
